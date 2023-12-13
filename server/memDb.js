3f0f4be2988d12bb27d814518309808c58e3b81e
Tobi <onipede89@gmail.com>
2023-12-14 00:44:39 +0100

    add Db skeleton

---
+++ server/memDb.js
@@ -0,0 +1,62 @@
+const data = {
+  skills: ["Node.js",
+'JavaScript',
+'React',
+'TypeScript',
+'Ruby',
+'Rails',
+'HTML5',
+'CSS3/Sass',
+'Bootstrap',
+'Express,'
+'Ejs',
+'PostgreSQL',
+'MongoDB,'
+'Git',
+'Cypress',
+'Python',
+'C',
+'Jest',
+'Wordpress',
+'jQuery',
+'Ajax',
+'Mocha & Chai',
+'RESTful API'],
+
+projects: [
+  {invoiceForge: { 
+    summary:'summary of Project',
+    picture: '/images/invoiceForge.png',
+    techstack: ['Nodejs', 'JavaScript', 'Express', 'React', 'Knexjs'],
+    url: '',
+    github: '',
+    date: '',
+ }},
+  {simplyPizza: {
+    summary:'',
+    picture: '',
+    techstack: ['Nodejs', 'JavaScript', 'Express', 'React', 'Knexjs'],
+    url: '',
+    github: '',
+  date: '',
+}},
+  {photolabs: {
+    summary:'',
+    picture: '',
+    techstack: ['Nodejs', 'JavaScript', 'Express', 'React', 'Knexjs'],
+    url: '',
+    github: '',
+    date: '',
+  }}
+],
+
+blog:{
+
+},
+
+
+}
+
+
+
+export default data;
